7cd22280eec1ac0bffbb6c0b6e34635e72dc3e07
Dima11221 <98055266+Dima11221@users.noreply.github.com>
2024-10-11 22:59:17 +0300

    Add files via upload

--- homework2/homework_funktions_2.js
+++ homework2/homework_funktions_2.js
@@ -1,30 +1,12 @@
 /*Задача 1*/
 console.log('Задача 1: Вывести сумму чисел от 1 до N')
 
-/*let sum = 0
-function letsum(n){
-    for(let i = 0; i<=n; i++){
-        sum += i;
-    }
-    return sum;
-}
-console.log(letsum(5))*/
-
-
 let letsum = (n, sum = 0) => {for(let i = 0; i <= n; i++){sum += i}{console.log('Сумма чисел от 1 до 5 будет равна ', sum)}}
 letsum(5)
 
 /*Задача 2*/
 console.log('----------------------------------')
 console.log('Задача 2: Вывести факториал числа N')
-
-/*let fact1 = 1
-function letfact1(i1) {
-    for(;i1>=1; fact1 *= i1--){
-    }
-    return fact1;
-}
-console.log(letfact1(5))*/
 
 let letfact = (i1, fact = 1) => {for(; i1>=1; i1--){fact *= i1}{console.log('Факториал числа 5 будет равен ', fact)}};
 letfact(5)
@@ -42,55 +24,29 @@ function even_odd_numbers(n){
 }
 even_odd_numbers(100)
 
-/*let i = 0
-function even_odd_numbers(even_num = 0, odd_num = 0) { 
-    while(i < 100) {
-        even_num++
-        odd_num++
-        i++
-        if (even_num % 2 == 0){
-            console.log('Четное число:', even_num)
-        }
-        if(odd_num % 2 != 0){
-            console.log('Нечетное число:', odd_num)
-        }
-}
-}
-console.log(even_odd_numbers());*/
-
 /*Задача 4*/
 console.log('----------------------------------')
 console.log('Задача 4: Вывести проверку на простое число')
 
-let n = 10
-let isPrime = true
-
-if (n === 1){
-    isPrime = false
-}
-else {
-    for (let i = 2; i < n; i++){
-        if (n % i == 0){
-            isPrime = false
-            break
+function isPrime_n(n, isPrime = true) {
+    if (n === 1){
+        isPrime = false}
+    else {
+        for (let i = 2; i < n; i++){
+            if (n % i == 0){
+                isPrime = false
+                break}
         }
     }
-}
 
-if (isPrime){console.log(n, '- простое число')}
-else {console.log(n, '- составное число')}
+    if (isPrime){console.log(n, '- простое число')}
+    else {console.log(n, '- составное число')}
+}
+isPrime_n(5)
 
 /*Задача 5*/
 console.log('----------------------------------')
 console.log('Задача 5: Вывести таблицу умножения для числа N')
-
-
-/*function multiplication(n1) {
-    for (let i = 1; i <= 10; i++) {
-        console.log(n1, 'умножить на ', i, ' равно ', n1*i) 
-}
-}
-multiplication(10)*/
 
 
 let multiplication = (n1) => {for(let i = 1; i <= 10; i++){console.log(n1, 'умножить на ', i, ' равно ', n1*i)}}
@@ -99,9 +55,6 @@ multiplication(10)
 /*Задача 6*/
 console.log('----------------------------------')
 console.log('Задача 6: Вывести сумму цифр числа N')
-
-/*let N = 322; 
-let sum1 = 0;*/
 
 function sum(N, sum1 = 0) {
     let digits = N.toString();
@@ -115,9 +68,6 @@ console.log(sum(555));
 /*Задача 7*/
 console.log('----------------------------------')
 console.log('Задача 7:Обратное число')
-
-/*let numb = 322
-let reversedNumb = 0*/
 
 function reversedNumb(numb, rev_Numb = 0) {
     while (numb > 0) {
@@ -133,12 +83,6 @@ console.log(reversedNumb(322))
 /*Задача 8*/
 console.log('----------------------------------')
 console.log('Задача 8:Количество цифр в числе N')
-
-/*function toStr(num) {
-    str = String(num)
-    return str.length
-}
-console.log(toStr(888888888))*/
 
 let toStr1 = (num1) => {{str1 = String(num1)}{return 'Количество цифр в числе '+ num1 + ' равна ' + str1.length}}
 console.log(toStr1(55555))
@@ -162,13 +106,6 @@ console.log(pallindrome(232));
 console.log('----------------------------------')
 console.log('Задача 10: Сумма квадратов чисел от 1 до N')
 
-/*function sqrt_numb(numbbb, sqrt = 0) {
-    for (let count1 = 1; count1 <= numbbb; count1++) {
-        sqrt += count1**2}
-    return sqrt
-}
-
-console.log(sqrt_numb(3))*/
 
 let sqrt_numb = (numbbb, sqrt = 0) => {for(let count1 = 1; count1 <= numbbb; count1 ++){sqrt += count1**2}{return 'Сумма квадратов чисел от 1 до ' + numbbb + ' равна ' + sqrt}}
 console.log(sqrt_numb(3))
